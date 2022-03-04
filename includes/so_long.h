e4e740edd7045cb8fca4862b327b106a10421b40
Souinia Ayoub <souiniaayoub@gmail.com>
2022-03-04 15:33:23 +0100

    SO_LONG ALMOST DONE

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,12 +6,12 @@
 /*   By: asouinia <asouinia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 02:46:57 by asouinia          #+#    #+#             */
-/*   Updated: 2022/03/04 14:52:41 by asouinia         ###   ########.fr       */
+/*   Updated: 2022/03/04 15:32:28 by asouinia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	SO_LONG_H
-#define	SO_LONG_H
+#ifndef SO_LONG_H
+# define SO_LONG_H
 # include <mlx.h>
 # include <stdio.h>
 # include <fcntl.h>
@@ -20,13 +20,13 @@
 # define BLOCK_SIZE 50
 # define BUFFER_SIZE 1
 //? XPM PATHS
-#define PLAYER_BLOCK "xpm/p.xpm"
-#define WALL_BLOCK "xpm/1.xpm"
-#define COIN_BLOCK "xpm/c-"
-#define DEATH_BLOCK "xpm/d-"
-#define BG_BLOCK "xpm/0.xpm"
-#define EXIT_OPEN_BLOCK "xpm/e_o.xpm"
-#define EXIT_CLOSE_BLOCK "xpm/e_c.xpm"
+# define PLAYER_BLOCK "xpm/p.xpm"
+# define WALL_BLOCK "xpm/1.xpm"
+# define COIN_BLOCK "xpm/c-"
+# define DEATH_BLOCK "xpm/d-"
+# define BG_BLOCK "xpm/0.xpm"
+# define EXIT_OPEN_BLOCK "xpm/e_o.xpm"
+# define EXIT_CLOSE_BLOCK "xpm/e_c.xpm"
 //? EVENT KEYS CODES
 # define KEY_UP 13
 # define KEY_DOWN 1
@@ -34,49 +34,49 @@
 # define KEY_RIGHT 2
 # define KEY_ESC 53
 
-#define KEY_ARROW_UP 126
-#define KEY_ARROW_DOWN 125
-#define KEY_ARROW_LEFT 123
-#define KEY_ARROW_RIGHT 124
+# define KEY_ARROW_UP 126
+# define KEY_ARROW_DOWN 125
+# define KEY_ARROW_LEFT 123
+# define KEY_ARROW_RIGHT 124
 //? ERRORS
-#define ERROR_MAP_NOT_FOUND 1 
-#define ERROR_MAP_NOT_VALID 2 
-#define ERROR_MAP_WIDTH 3
-#define ERROR_MAP_BORDER 4
-#define ERROR_MAP_PLAYER 5
-#define ERROR_MAP_EXIT 6
-#define ERROR_MAP_COINS 7
-#define ERROR_MAP_FOUND_FORBIDDEN_OBJECT 8
-#define ERROR_MAP_NOT_BER_EXTENTION 9
-#define ERROR_GAME_IS_NULL 100
+# define ERROR_MAP_NOT_FOUND 1 
+# define ERROR_MAP_NOT_VALID 2 
+# define ERROR_MAP_WIDTH 3
+# define ERROR_MAP_BORDER 4
+# define ERROR_MAP_PLAYER 5
+# define ERROR_MAP_EXIT 6
+# define ERROR_MAP_COINS 7
+# define ERROR_MAP_FOUND_FORBIDDEN_OBJECT 8
+# define ERROR_MAP_NOT_BER_EXTENTION 9
+# define ERROR_GAME_IS_NULL 100
 
 typedef struct s_mlx
 {
-	void *mlx;
-	void *mlx_win;
-} t_mlx;
+	void	*mlx;
+	void	*mlx_win;
+}				t_mlx;
 
 typedef struct s_game
 {
-	char **map;
-	int	game;
-	int c_i;
-	int c_i_direction;
-	int death_xpm_index;
-	int death_xpm_index_direction;
-	int death_exist;
-	int d_x;
-	int d_y;
-	int count_move;
-	int height;
-	int width;
-	int p_x;
-	int p_y;
-	int e_x;
-	int e_y;
-	int n_coins;
-	t_mlx *mlx;
-} t_game;
+	char	**map;
+	int		game;
+	int		c_i;
+	int		c_i_direction;
+	int		death_xpm_index;
+	int		death_xpm_index_direction;
+	int		death_exist;
+	int		d_x;
+	int		d_y;
+	int		count_move;
+	int		height;
+	int		width;
+	int		p_x;
+	int		p_y;
+	int		e_x;
+	int		e_y;
+	int		n_coins;
+	t_mlx	*mlx;
+}				t_game;
 
 //? validate_map_structure.c
 int		validate_map(t_game *game);
@@ -89,7 +89,7 @@ int		validate_map_exit(t_game *game);
 int		validate_map_coins(t_game *game);
 int		validate_map_other_chars(t_game *game);
 //? init_game.c
-t_game *init_game(char *map_path);
+t_game	*init_game(char *map_path);
 int		free_game(t_game **game);
 void	print_map(t_game *game);
 void	set_init_death(t_game *game);
@@ -106,9 +106,9 @@ int		can_move_death(t_game *game, int direction);
 //? loops.c
 int		loops(t_game *game);
 //? endgame.c
-void win_game(t_game *game);
-void lost_game(t_game *game);
-void close_game(t_game *game);
+void	win_game(t_game *game);
+void	lost_game(t_game *game);
+void	close_game(t_game *game);
 //? errors2.c
 void	catch_error_map_border(void);
 void	catch_error_map_player(void);
@@ -120,5 +120,4 @@ void	catch_error_map_not_valid(void);
 void	catch_error_map_not_ber_extention(void);
 void	catch_error_game_is_null(void);
 void	catch_error_map_width(void);
-
 #endif
